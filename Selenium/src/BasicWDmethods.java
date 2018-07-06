318cdeb404c9026dd707a7af54411758cfcb1ab0
RakeshKumarA <RakeshKumarA@github.com>
2018-07-06 10:53:04 -0500

    Basic Web Methods

---
+++ Selenium/src/BasicWDmethods.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicWDmethods {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/Users/ramritku/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		//System.out.println(driver.getPageSource());
+		System.out.println(driver.getClass());
+		System.out.println(driver.getWindowHandle());
+		driver.close();
+
+	}
+
+}
